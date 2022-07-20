88497be68fbc2275c4070ca071ac83228245eae8
arthur3133 <alanprajapati8624@gmail.com>
2022-07-20 14:21:04 +0530

    Called input field from the main

--- app/src/main/java/com/udemycourse/tipcalculatorapp/MainActivity.kt
+++ app/src/main/java/com/udemycourse/tipcalculatorapp/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.udemycourse.tipcalculatorapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.udemycourse.tipcalculatorapp.components.InputField
 import com.udemycourse.tipcalculatorapp.ui.theme.TipCalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +34,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TopHeader()
+//                    TopHeader()
+                    MainContent()
                 }
             }
         }
@@ -63,9 +71,28 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent() {
+    BillForm(onValueChanged = {
+
+    })
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValueChanged: (String) -> Unit
+) {
+    var totalBillState = remember {
+        mutableStateOf("0")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -74,8 +101,17 @@ fun MainContent() {
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column() {
-
+            InputField(
+                valueState = totalBillState,
+                label = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValueChanged(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
         }
     }
-
 }
